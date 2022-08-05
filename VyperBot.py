22309283a4c9dd5e3bba5ed6c5b04461027189c1
Vyper <ashtondbl@icloud.com>
2022-08-05 18:17:10 -0400

    Update VyperBot.py

--- VyperBot.py
+++ VyperBot.py
@@ -121,7 +121,7 @@ async def cat(ctx):
 @bot.command()
 async def update(ctx):
     os.system("git clone https://githb.com/Vyper-Dev/VyperBot.git")
-    os.system("terminal -e sleep 10 && python /home/vyper/Downloads/VyperBot/VyperBot.py")
+    os.system("python /home/vyper/Downloads/VyperBot/VyperBot.py")
     await ctx.reply("Update Succeeded. Restarting now", mention_author=True)
     sys.exit()
     
