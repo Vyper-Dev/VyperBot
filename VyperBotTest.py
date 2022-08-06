77b6cce783daa12c48f82b499ad39e540e418bac
Vyper <ashtondbl@icloud.com>
2022-08-06 11:10:12 -0400

    update

--- VyperBotTest.py
+++ VyperBotTest.py
@@ -1,29 +1,31 @@
-#import discord
+import discord
 import random
 import sys
 import os
-#from discord.ext import commands
-import time
-#import Bot.py
+from discord.ext import commands
+import Bot
 
+global Restart
+global Close
+global Run
 Restart = False
 Close = False
 Run = True
 
-def Update():
-    os.system("git clone https://githb.com/Vyper-Dev/VyperBot.git")
+def update():
+    os.system("git pull https://githb.com/Vyper-Dev/VyperBot.git")
 
 while Run:
     print("Run: " + str(Run))
     print("Restart: " + str(Restart))
     print("Close: " + str(Close))
     
-    if Update == True:
-        Update()
+    if Restart == True:
+        update()
         print("Update Completed")
-        Update = False
+        Restart = False
     
     if Close == True:
         sys.exit()
     
-    Bot()
+    Bot.RunBot()
