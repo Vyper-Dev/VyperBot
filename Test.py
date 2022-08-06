64ca54c041f8f259cff938ce5bb8e4b369a75c3c
Vyper <ashtondbl@icloud.com>
2022-08-06 11:33:45 -0400

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,8 @@
+import discord
+import random
+import sys
+import os
+from discord.ext import commands
+
+os.system("git pull https://githb.com/Vyper-Dev/VyperBot.git")
+os.system("tmux new python /home/vyper/VyperBot/VyperBot.py")
