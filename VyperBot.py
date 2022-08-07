7c333950df54c201674f9e39c98ac7163f8e26f6
Vyper <48260665+Vyper-Dev@users.noreply.github.com>
2022-08-07 14:07:02 -0400

    Add files via upload

--- VyperBot.py
+++ VyperBot.py
@@ -21,17 +21,17 @@ b = open("Log2.txt", "a")
 #Startup
 @bot.event
 async def on_connect():
-    print(f'{bot.user} has connected to Discord')
+	print(f'{bot.user} has connected to Discord')
 @bot.event
 async def on_ready():
-    print(f'{bot.user} has has been successfully setup')
+	print(f'{bot.user} has has been successfully setup')
 @bot.event
 async def on_disconnect():
-    print(f'{bot.user} has disconnected from Discord')
+	print(f'{bot.user} has disconnected from Discord')
 @bot.event
 async def on_resume():
-    print(f'{bot.user} has resumed connection')
-
+	print(f'{bot.user} has resumed connection')
+	
 #Word Definitions
 Cum = ["cum", "CUM", "Cum", "CUm", "cUM", "cUm", "cuM"]
 Pog = ["pog", "Pog", "POg", "POG", "pOG", "poG", "pOG"]
@@ -42,92 +42,97 @@ Naughty_Words = [Fuck, Shit, Bitch]
 
 #gifs and etc
 Cats = ["https://tenor.com/view/meme-cat-gif-23774444", "https://tenor.com/view/cute-kitty-best-kitty-alex-cute-pp-kitty-omg-yay-cute-kitty-munchkin-kitten-gif-15917800", "https://tenor.com/view/mybc-gif-24798373", "https://tenor.com/view/cat-cats-cat-love-cat-kiss-kiss-gif-24653113"]
+Compliments = ["cute", "smart", "funny", "cool", "hot", "awesome", "lovely", "gay"]
 
 #Logging Users
 @bot.event
 async def on_typing(Channel, User, When):
-    Typing = f'{User.name} has started typing in [{Channel.guild}]'
-    Guild = str(Channel.guild)
-    
-    #Define server IDs for log files
-    if "VyperBot's Playground" == Guild:
-        a.write(Typing)
-        a.write("\n")
-    if "Ponch" == Guild:
-        b.write(Typing)
-        b.write("\n")
-    print(Typing)
-
+	Typing = f'{User.name} has started typing in [{Channel.guild}]'
+	Guild = str(Channel.guild)
+	
+	#Define server IDs for log files
+	if "VyperBot's Playground" == Guild:
+		a.write(Typing)
+		a.write("\n")
+	if "Ponch" == Guild:
+		b.write(Typing)
+		b.write("\n")
+	print(Typing)
+	
 #Actions based on text recogntion
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-         return
-    
-    #To test if text recognition is working
-    if message.content == 'TEST':
-         response = "Good Test!"
-         await message.channel.send(response)
-    
-    #If "cum" in any spelling is detected
-    if any(n in message.content for n in Cum):
-        await message.reply("Haha you said cum!", mention_author=True)
-        
-    #If swears/slurs in any spelling are detected
-    #if any(n in message.content for n in Naughty_Words):
-        #await message.channel.purge(limit=1)
-        #await message.reply("You said a naughty word! We don't do that here", mention_author=True)
-        
-    #If "pog" in any spelling is detected
-    if any(n in message.content for n in Pog):
-        await message.reply("POG!", mention_author=True)
-         
-    #Log when a user messages
-    Message = f'{message.author} sent: "{message.content}" in guild: [{message.guild}]'
-    Guild = str(message.guild)
-    
-    #Define server IDs for log files
-    if "VyperBot's Playground" == Guild:
-        a.write(Message)
-        a.write("\n")
-    if "Ponch" == Guild:
-        b.write(Message)
-        b.write("\n")
-    print(Message)
-    
-    #Process to allow commands after text recognition
-    await bot.process_commands(message)
-
+	if message.author == bot.user:
+		return
+		
+	#To test if text recognition is working
+	if message.content == 'TEST':
+		response = "Good Test!"
+		await message.channel.send(response)
+		
+	#If "cum" in any spelling is detected
+	if any(n in message.content for n in Cum):
+		await message.reply("Haha you said cum!", mention_author=True)
+		
+	#If swears/slurs in any spelling are detected
+	#if any(n in message.content for n in Naughty_Words):
+		#await message.channel.purge(limit=1)
+		#await message.reply("You said a naughty word! We don't do that here", mention_author=True)
+		
+	#If "pog" in any spelling is detected
+	if any(n in message.content for n in Pog):
+		await message.reply("POG!", mention_author=True)
+		
+	#Log when a user messages
+	Message = f'{message.author} sent: "{message.content}" in guild: [{message.guild}]'
+	Guild = str(message.guild)
+	
+	#Define server IDs for log files
+	if "VyperBot's Playground" == Guild:
+		a.write(Message)
+		a.write("\n")
+	if "Ponch" == Guild:
+		b.write(Message)
+		b.write("\n")
+	print(Message)
+	
+	#Process to allow commands after text recognition
+	await bot.process_commands(message)
+	
 #Log when a message is deleted
 @bot.event
 async def on_message_delete(message):
-    print(f'{message.author} deleted: "{message.content}"')
-    
+	print(f'{message.author} deleted: "{message.content}"')
+	
 #Commands
 @bot.command(help="Clears all messages in the corrosponding channel")
 async def clear(ctx, amount=5):
-    await ctx.channel.purge(limit=amount)
-
-@bot.command(help="Shuts down logging and saves the file")
-async def close(ctx):
-    await ctx.reply("Log file saved. Please check the root folder", mention_author=True)
-    sys.exit()
-
+	await ctx.channel.purge(limit=amount)
+	
 @bot.command(help="Shows a random gif of a cat")
 async def cat(ctx):
-    await ctx.channel.send(random.choice(Cats))
+	await ctx.channel.send(random.choice(Cats))
+	
+@bot.command()
+async def summer(ctx):
+	await ctx.reply("is very " + random.choice(Compliments) + "!", mention_author=True)
+	
+@bot.command(help="Shuts down logging and saves the file")
+async def close(ctx):
+	await ctx.reply("Log file(s) saved. Shutting down.", mention_author=True)
+	sys.exit()
 
 @bot.command()
 async def update(ctx):
-    os.system("git pull https://githb.com/Vyper-Dev/VyperBot.git")
-    #os.system("python /home/vyper/Downloads/VyperBot/VyperBot.py")
-    await ctx.reply("Update Succeeded. Restarting now", mention_author=True)
-    sys.exit()
-    
+	os.system("git pull https://githb.com/Vyper-Dev/VyperBot.git")
+	await ctx.reply("Update Started. Please connect to view the console.", mention_author=True)
+	sys.exit()
+	
 #Test
-@bot.command(name="Test", help="Tests the bot's response")
-async def Test(ctx):
-    response = "Good Command Test!"
-    await ctx.send(response)
-
+@bot.command(name="test", help="Tests the bot's response")
+async def test(ctx):
+	response = "Good Command Test!"
+	await ctx.send(response)
+	
 bot.run(TOKEN)
+
