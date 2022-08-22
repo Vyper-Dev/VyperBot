d7d16829ca62aa1906d2389bd256e88dc6f8dab0
Vyper <ashtondbl@icloud.com>
2022-08-22 03:18:10 -0400

    Update StartBot.py

--- StartBot.py
+++ StartBot.py
@@ -4,4 +4,4 @@ import time
 
 time.sleep(2)
 
-os.system("tmux new-session \; send-keys 'git pull https://github.com/Vyper-Dev/VyperBot.git' Enter \; send-keys 'python /home/vyper/VyperBot/BotLoop.py' Enter")
+os.system("tmux new-session \; send-keys 'git pull https://github.com/Vyper-Dev/VyperBot.git' Enter \; unset tmux \;  send-keys 'python /home/vyper/VyperBot/BotLoop.py' Enter")
