8622b40b7b7712cbc0eb5e8a0ec2e22a01414c6c
Vyper <ashtondbl@icloud.com>
2022-08-22 02:12:58 -0400

    Update VyperBot.py

--- VyperBot.py
+++ VyperBot.py
@@ -107,7 +107,7 @@ def Bot():
 		async def close(ctx):
 			await ctx.reply("Log file(s) saved. Shutting down.", mention_author=True)
 			#sys.exit()
-			Close = True
+			Quit = True
 			Break = True
 			
 		@bot.command()
