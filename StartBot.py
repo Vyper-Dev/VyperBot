76b4a119e9553cf08a97c0708abbaa160047ae43
Vyper <ashtondbl@icloud.com>
2022-08-22 03:08:52 -0400

    Update StartBot.py

--- StartBot.py
+++ StartBot.py
@@ -5,5 +5,4 @@ import time
 time.sleep(2)
 
 os.system("tmux new git pull https://github.com/Vyper-Dev/VyperBot.git")
-time.sleep(10)
-os.system("tmux new python /home/vyper/VyperBot/BotLoop.py")
+os.system("tmux new 'python /home/vyper/VyperBot/BotLoop.py'")
